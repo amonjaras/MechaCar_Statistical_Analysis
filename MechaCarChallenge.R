2a00391b6fa62f6926601eb75176f784c5001d24
amonjaras <100452376+amonjaras@users.noreply.github.com>
2022-06-19 19:52:44 -0400

    Adding Resources and Deliverable 1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,12 @@
+## Deliverable 1
+# Use the library() function to load the dplyr package
+library(dplyr)
+
+# Import and read in the MechaCar_mpg.csv file as a dataframe.
+mpg_data <- read.csv(file = 'Resources/MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+
+# Perform linear regression using the lm() function
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data = mpg_data)
+
+# Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data = mpg_data))
